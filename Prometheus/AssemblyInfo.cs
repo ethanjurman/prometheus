4c1b010725955c6d0417a03ea52276bd3f0292d4
nizmo400z <jrg1541@rit.edu>
2013-08-28 18:54:55 -0400

    changed assembly version

--- Prometheus/AssemblyInfo.cs
+++ Prometheus/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Prometheus")]
 [assembly: AssemblyDescription("Creator of HUMANS, duh!")]
-[assembly: AssemblyConfiguration("0.1")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Ethan Jurman & Jordan Gundlach")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
