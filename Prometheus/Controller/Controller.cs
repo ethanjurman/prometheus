5703a5664f63312d80f24bf545b97697d1f6516f
Ethan H. Jurman <ehj2229@rit.edu>
2013-08-28 20:21:12 -0400

    added controller

---
+++ Prometheus/Controller/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Prometheus.Model;
+
+namespace Prometheus.Controller
+{
+    class Controller : System.Collections.CollectionBase
+    {
+        public void Add(Event element)
+        {
+            List.Add(element);
+        }
+
+        public Event Pop()
+        {
+            var element = List[Count - 1];
+            List.RemoveAt(Count - 1);
+            // event.execute
+            return (Event)element;
+        }
+    }
+}
