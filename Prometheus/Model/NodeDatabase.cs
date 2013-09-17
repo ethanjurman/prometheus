1e2f8cee348c9cbb4ad3177e6612769101213bea
nizmo400z <jrg1541@rit.edu>
2013-09-17 17:04:25 -0400

    changed the name of the database from Database to NodeDatabase

--- Prometheus/Model/NodeDatabase.cs
+++ Prometheus/Model/NodeDatabase.cs
@@ -5,10 +5,10 @@ using System.Text;
 
 namespace Prometheus.Model
 {
-    public class Database
+    public class NodeDatabase
     {
         Dictionary<string, SortedSet<Node>> nodes;
-        public Database()
+        public NodeDatabase()
         {
             nodes = new Dictionary<string, SortedSet<Node>>();
         }
