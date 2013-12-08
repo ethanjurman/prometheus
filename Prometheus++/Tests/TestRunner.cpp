bd2b451a92f093683d17188a23715b9b1e1e3291
nizmo400z <nizmo400z@gmail.com>
2013-12-08 15:15:36 -0500

    Made some updates to the TestRunner, and modified teh EvalNode isNumber() function, since c++11 doens't support regex...

--- Prometheus++/Tests/TestRunner.cpp
+++ Prometheus++/Tests/TestRunner.cpp
@@ -1,14 +1,41 @@
 #include "TestRunner.h"
+#include <iostream>
+using namespace std;
 
 void TestRunner::addTest(void (*func)()) {
   tests.push_back(func);
 }
 
 void TestRunner::runTests() {
-  /*for(std::vector<void (*)()>::iterator i = tests.begin(); i != tests.end(); ++i)
-	*i();*/
-  
-  // eww...this looks like java, since I'm not using iter
-  for(int i = 0; i < tests.size(); ++i)
-	tests[i]();
+  for(int i = 0; i < tests.size(); ++i) {
+	try {
+	  tests[i]();
+	} catch(AssertException& e) {
+	  cerr <<  e.getMessage() << endl;
+	}
+  }
+}
+
+void TestRunner::assertTrue(string message, bool value) {
+  if(!value) {
+	throw AssertException(message);
+  }
+}
+
+void TestRunner::assertTrue(bool value) {
+  if(!value) {
+	throw AssertException();
+  }
+}
+
+void TestRunner::assertFalse(string message, bool value) {
+  if(value) {
+	throw AssertException(message);
+  }
+}
+
+void TestRunner::assertFalse(bool value) {
+  if(value) {
+	throw AssertException();
+  }
 }
